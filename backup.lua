cec2d6e3222e611ff9e0424d8fbb59702d28b8e9
Hugo O. Rivera <hugo@roguh.com>
2023-04-13 02:28:20 -0600

    Rename module to backup in backup.lua for cohesion with README
    This way users can modify either their visrc or backup.lua directly with the same examples shown in README.md

--- backup.lua
+++ backup.lua
@@ -1,26 +1,26 @@
-local module = {}
+local backup = {}
 
 
 -- Return the backup path concatenated with the filename, replace / with %
-module.entire_path_with_double_percentage_signs = function(backup_dir, path)
+backup.entire_path_with_double_percentage_signs = function(backup_dir, path)
   return backup_dir .. "/" .. string.gsub(path, "/", "%%")
 end
 
 
 -- Return the backup path concatenated with the filename, replace / with %
 -- and append the current time using time_format
-module.entire_path_with_double_percentage_signs_and_timestamp = function(backup_dir, path)
-  return backup_dir .. "/" .. os.date(module.time_format) .. string.gsub(path, "/", "%%")
+backup.entire_path_with_double_percentage_signs_and_timestamp = function(backup_dir, path)
+  return backup_dir .. "/" .. os.date(backup.time_format) .. string.gsub(path, "/", "%%")
 end
 
 
 -- Before saving the file, copy the current contents of the file to a backup file
 vis.events.subscribe(vis.events.FILE_SAVE_PRE, function (file, path)
-  if file.size > module.byte_limit then
+  if file.size > backup.byte_limit then
     return
   end
 
-  local backup_path = module.get_fname(module.directory, path)
+  local backup_path = backup.get_fname(backup.directory, path)
   
   local backup_file = io.open(backup_path, "w")
   local current_file = io.open(path)
@@ -37,15 +37,15 @@ end)
 
 
 -- Set defaults
-module.directory = os.getenv("HOME") .. "/.vis-backups" 
+backup.directory = os.getenv("HOME") .. "/.vis-backups" 
 
-module.get_fname = module.entire_path_with_double_percentage_signs
+backup.get_fname = backup.entire_path_with_double_percentage_signs
 
-module.time_format = "%H-%M-"
+backup.time_format = "%H-%M-"
 
 -- Do not make backups if the file is greater than this
 -- 1MB by default
-module.byte_limit = 1000000
+backup.byte_limit = 1000000
 
 
-return module
+return backup
